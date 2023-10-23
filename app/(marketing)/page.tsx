c7753409bf1d0af1f96369a83d2eb1d5658b1e82
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-10-23 11:18:26 +0500

    handled marketing

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import Heading from "./_components/heading";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col px-6 pb-10 items-center justify-center md:justify-start text-center gap-y-8 flex-1">
+        <Heading />
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
