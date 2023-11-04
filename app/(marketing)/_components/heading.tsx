c40714b2d189ba5951f1c3912902ee5c94965774
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-11-04 15:33:05 +0500

    final page completed

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,8 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
-const Heading = () => {
+export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -21,5 +20,3 @@ const Heading = () => {
     </div>
   );
 };
-
-export default Heading;
