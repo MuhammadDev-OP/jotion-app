ba3cf98b5191c5e80147f8804fc4cf668c03cf4c
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-11-24 23:34:00 +0500

    adsdsadasasd

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,5 @@
+const DocumentsPage = () => {
+  return <div>This is Protected DocumentsPage</div>;
+};
+
+export default DocumentsPage;
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
