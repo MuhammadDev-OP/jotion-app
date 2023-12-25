eed3b03458c7f15e7496e33b3f491875d8d46ae5
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-12-25 16:01:13 +0500

    modified

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -13,7 +13,7 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans. Unified, Welcome
+        Your Ideas, Documents, & Plans. Unified, Welcome{" "}
         <span className="underline">Jotion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
