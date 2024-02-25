20fe8e73991462b50b4176e59907085b82bc4519
Muhammad Bangash <muhammadbangash190@gmail.com>
2024-02-25 10:54:38 +0500

    Bismillah

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+import Image from "next/image";
+
 const DocumentsPage = () => {
-  return <div>This is Protected DocumentsPage</div>;
+  return (
+    <div className="h-screen flex justify-center items-center flex-col space-y-4">
+      <div className="mx-auto">
+        <Image
+          src="/empty.png"
+          height={300}
+          width={300}
+          alt={"empty"}
+          className="dark:hidden"
+        />
+      </div>
+      <div className="mx-auto">
+        <Image
+          src="/empty-dark.png"
+          height={300}
+          width={300}
+          alt={"empty"}
+          className="dark:black"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentsPage;
