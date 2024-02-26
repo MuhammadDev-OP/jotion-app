b5f6bea35dd2356d0a45a0064918bf58ec8d4192
Muhammad Bangash <muhammadbangash190@gmail.com>
2024-02-26 23:48:44 +0500

    Styling corrected

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,8 +3,26 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = async () => {
+    try {
+      const document = await create({ title: "Untitled" });
+
+      toast.success("New note created!"); // Assuming successful document creation
+
+      // Handle success case using the document data, if applicable
+    } catch (error) {
+      toast.error("Failed to create a new note."); // Log the error for debugging
+    }
+  };
+
   return (
     <div className="h-full flex flex-col justify-center items-center space-y-4">
       <Image
@@ -24,7 +42,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Jotion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -22,7 +22,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
-      <main className="">{children}</main>
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
     </div>
   );
 };
